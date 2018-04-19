94cde1a18ecafad06359744aa4d7e50646750bd1
Eric jean <ejean@dixit-productions.com>
2018-04-19 16:55:59 +0200

    maj arbo
    maj arbo

--- Plugins/wordapp-mobile-app/themes/wordappjqmobile/sidebar.php
+++
@@ -1,9 +0,0 @@
-
-
-<div data-role="panel" id="mypanelRight" data-position-fixed="false" data-position="right" data-display="reveal">
-<div  class="widget-area" role="complementary">	<?php
-	
- dynamic_sidebar( 'wordapp-mobile-sidebar-right' ); 
-			?>
-</div>
-</div>
